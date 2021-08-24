b51d85d863220f6c448af611d11bdd6c273f94c1
Victor-Lutz <83316369+Victor-Lutz@users.noreply.github.com>
2021-08-23 22:28:07 -0300

    Delete lib directory

--- lib/provider/weather_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/dio.dart';
-
-Future<Map> getData() async{
-  var response =
-      await Dio().get("https://api.hgbrasil.com/weather?key=f6100a1f&woeid=455823");
-
-  return response.data;
-}
--- lib/style.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/painting.dart';
-
-Color grayTheme = Color(0xFF2E3643);
